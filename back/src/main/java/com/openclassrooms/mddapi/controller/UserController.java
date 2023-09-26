5cef952009edbc65bc2d6adc1fa32a0f2b4f6e9e
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-09-26 10:06:23 +0200

    :truck: User endpoint added

--- back/src/main/java/com/openclassrooms/mddapi/controller/UserController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/UserController.java
@@ -1,2 +1,21 @@
-package com.openclassrooms.mddapi.controller;public class UserController {
+package com.openclassrooms.mddapi.controller;
+
+import com.openclassrooms.mddapi.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/user")
+public class UserController {
+    private UserService userService;
+    public UserController (UserService userService){
+        this.userService = userService;
+    }
+    @GetMapping("/{id_user}")
+    public ResponseEntity<?> getUserById(@PathVariable(name = "id_user")int id_user){
+        return userService.user_id(id_user);
+    }
 }
