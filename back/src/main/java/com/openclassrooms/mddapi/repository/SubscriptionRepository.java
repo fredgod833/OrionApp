068b03c6dcf5222dfb739d936e5cecb93761fb1b
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-09-26 10:16:40 +0200

    :card_file_box: Database access by SubscriptionRepository

--- back/src/main/java/com/openclassrooms/mddapi/repository/SubscriptionRepository.java
+++ back/src/main/java/com/openclassrooms/mddapi/repository/SubscriptionRepository.java
@@ -1,2 +1,11 @@
-package com.openclassrooms.mddapi.repository;public interface SubscriptionList {
+package com.openclassrooms.mddapi.repository;
+
+import com.openclassrooms.mddapi.model.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@EnableJpaRepositories
+public interface SubscriptionRepository extends JpaRepository<Subscription, Integer> {
 }
