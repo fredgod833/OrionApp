541549deed6d211db94384ee99ba7d8521e4c4ca
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-09-26 10:21:32 +0200

    :necktie: User service layer added

--- back/src/main/java/com/openclassrooms/mddapi/service/UserService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/UserService.java
@@ -1,2 +1,9 @@
-package com.openclassrooms.mddapi.service;public class UserService {
+package com.openclassrooms.mddapi.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+    ResponseEntity<?> user_id(int id_user);
 }
--- back/src/main/java/com/openclassrooms/mddapi/service/UserServiceImpl.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/UserServiceImpl.java
@@ -1,2 +1,32 @@
-package com.openclassrooms.mddapi.service;public interface UserServiceImpl {
+package com.openclassrooms.mddapi.service;
+
+import com.openclassrooms.mddapi.model.User;
+import com.openclassrooms.mddapi.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public ResponseEntity<?> user_id(int id_user) {
+        User user_identity = new User();
+
+        try {
+            user_identity = userRepository.findById(id_user).orElse(null);
+            if (user_identity == null) {
+                return null;
+            }
+            return ResponseEntity.ok(user_identity);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    // TODO: 22/09/2023 Login user
+    // TODO: 22/09/2023 Create user endPoint
 }
