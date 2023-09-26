7843ac76474d12714137ed57c05bfc308b6679e6
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-09-26 10:20:16 +0200

    :necktie: Subject service layer added

--- back/src/main/java/com/openclassrooms/mddapi/service/SubjectService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/SubjectService.java
@@ -1,13 +1,16 @@
 package com.openclassrooms.mddapi.service;
 
 import com.openclassrooms.mddapi.model.Subject;
-import lombok.AllArgsConstructor;
+import com.openclassrooms.mddapi.model.dto.SubjectDto;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public interface SubjectService {
     List<Subject> getSubjectList();
+    Subject getSubjectById(int id_subject);
+    List<SubjectDto> findSubjectDtoList();
+
+    List<SubjectDto> subscribedList();
 }
--- back/src/main/java/com/openclassrooms/mddapi/service/SubjectServiceImpl.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/SubjectServiceImpl.java
@@ -1,33 +1,105 @@
 package com.openclassrooms.mddapi.service;
-
 import com.openclassrooms.mddapi.model.Subject;
+import com.openclassrooms.mddapi.model.dto.SubjectDto;
 import com.openclassrooms.mddapi.repository.SubjectRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 @Service
-@AllArgsConstructor
 public class SubjectServiceImpl implements SubjectService {
 
     private final SubjectRepository subjectRepository;
+    public SubjectServiceImpl(SubjectRepository subjectRepository){
+        this.subjectRepository = subjectRepository;
+    }
 
-    public List<Subject> getSubjectList(){
+    public List<SubjectDto> subscribedList(){
         List<Subject> subjectList = new ArrayList<>();
-        try{
-           subjectList = subjectRepository.findAll();
+        List<SubjectDto> subjectDtoList = new ArrayList<>();
+        try {
+            subjectList = getSubjectList();
 
-           if (!subjectList.isEmpty()){
-               return subjectList;
-           }
+            for (Subject subject: subjectList) {
+                if (subject.getIsSubscribed().equals(true)){
+                    SubjectDto subjectDto = toDto.apply(subject);
+
+                    subjectDtoList.add(subjectDto);
+
+                    return subjectDtoList;
+                }
+            }
         }catch (Exception e){
             throw new RuntimeException(e.getMessage());
         }
+        return subjectDtoList;
+    }
+    public List<SubjectDto> findSubjectDtoList(){
+        List<Subject> subjectList = new ArrayList<>();
+        List<SubjectDto> subjectDtoList = new ArrayList<>();
+
+        try {
+            subjectList = subjectRepository.findAll();
+
+            if (subjectList.isEmpty()){
+                throw new RuntimeException("Subject list may be empty");
+            }
+
+            for (Subject subject: subjectList) {
+                SubjectDto subjectDto = SubjectDto.builder()
+                        .idSubject(subject.getIdSubject())
+                        .title(subject.getTitle())
+                        .description(subject.getDescription())
+                        .isSubscribed(subject.getIsSubscribed())
+                        .build();
+
+                subjectDtoList.add(subjectDto);
+            }
+        }catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+        return subjectDtoList;
+    }
+
+    public List<Subject> getSubjectList(){
+
+        List<Subject> subjectList  = new ArrayList<>();
+
+        try {
+            subjectList = subjectRepository.findAll();
+
+            if (subjectList.isEmpty()){
+                throw new RuntimeException("List of subject may be Empty!!!");
+            }
+
+        }catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+
         return subjectList;
     }
+
+    public Subject getSubjectById(int id_subject){
+        Subject subject = new Subject();
+        try {
+            subject = subjectRepository.findById(id_subject).orElse(null);
+
+            if (subject== null){
+                return null;
+            }
+        }catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+        return subject;
+    }
+    private final Function<Subject, SubjectDto> toDto = (subject) ->
+            SubjectDto.builder()
+                    .idSubject(subject.getIdSubject())
+                    .title(subject.getTitle())
+                    .description(subject.getDescription())
+                    .isSubscribed(subject.getIsSubscribed())
+                    .build();
 
 }
