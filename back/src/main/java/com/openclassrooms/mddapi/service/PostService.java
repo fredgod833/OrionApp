5244e6e7582bdab11bcf882523f9bcddb451e517
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-09-26 10:19:42 +0200

    :necktie: Post service layer added

--- back/src/main/java/com/openclassrooms/mddapi/service/PostService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/PostService.java
@@ -1,2 +1,14 @@
-package com.openclassrooms.mddapi.service;public interface PostService {
+package com.openclassrooms.mddapi.service;
+
+import com.openclassrooms.mddapi.model.Post;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface PostService {
+    List<Post> postList();
+    String createPost(Post post);
+    ResponseEntity<?> findPostById(int post_id);
 }
--- back/src/main/java/com/openclassrooms/mddapi/service/PostServiceImpl.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/PostServiceImpl.java
@@ -1,2 +1,104 @@
-package com.openclassrooms.mddapi.repository;public class PostService {
+package com.openclassrooms.mddapi.service;
+
+import com.openclassrooms.mddapi.model.Post;
+import com.openclassrooms.mddapi.model.Subject;
+import com.openclassrooms.mddapi.model.dto.SubjectDto;
+import com.openclassrooms.mddapi.repository.PostRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PostServiceImpl implements PostService {
+    private final PostRepository postRepository;
+    private final SubjectService subjectService;
+
+    public PostServiceImpl(PostRepository postRepository, SubjectService subjectService) {
+        this.postRepository = postRepository;
+        this.subjectService = subjectService;
+    }
+
+    public List<Post> postList() {
+        List<Post> postArray = new ArrayList<>();
+
+        try {
+            postArray = postRepository.findAll();
+
+            if (postArray.isEmpty()) {
+                throw new RuntimeException("List of post could be empty!!!");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        return postArray;
+    }
+
+    public ResponseEntity<?> findPostById(int post_id) {
+        Post post = new Post();
+
+        try {
+            post = postRepository.findById(post_id).orElse(null);
+
+            if (post == null) {
+                throw new RuntimeException("Post may be null");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+        return ResponseEntity.ok(post);
+    }
+
+    @Override
+    public String createPost(Post post) {
+
+        try {
+            //choose a subject in the subject list
+            List<SubjectDto> subjectDtoList = new ArrayList<>();
+            subjectDtoList = subjectService.findSubjectDtoList();
+
+            //verify subject is present in the subject list
+            if (subjectDtoList.isEmpty()) {
+                throw new RuntimeException("SubjectDto list may be empty");
+            }
+
+            //verify if post is not null
+            if (post == null) {
+                return null;
+            }
+
+            //identify the subject of choice
+            for (SubjectDto subject : subjectDtoList) {
+
+                //build post
+                if (post.getSubject().getIdSubject() == subject.getIdSubject()) {
+
+                    Subject sub = Subject.builder()
+                            .idSubject(subject.getIdSubject())
+                            .title(subject.getTitle())
+                            .description(subject.getDescription())
+                            .isSubscribed(subject.getIsSubscribed())
+                            .build();
+                    Post buildPost = Post.builder()
+                            .id_post(post.getId_post())
+                            .title(post.getTitle())
+                            .content(post.getContent())
+                            .author(post.getAuthor())
+                            .date(post.getDate())
+                            .comments(post.getComments())
+                            .subject(sub)
+                            .build();
+
+                    //create post
+                    postRepository.save(buildPost);
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        //return message if post was created
+        return "Post created !!!";
+    }
 }
