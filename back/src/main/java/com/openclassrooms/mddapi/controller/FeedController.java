4fdca30c2262549f04c76ac6da72c1a2f0d1260c
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-09-26 10:03:37 +0200

    :truck: Feeds endpoints added

--- back/src/main/java/com/openclassrooms/mddapi/controller/FeedController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/FeedController.java
@@ -1,2 +1,32 @@
-package com.openclassrooms.mddapi.controller;public class FeedController {
+package com.openclassrooms.mddapi.controller;
+
+import com.openclassrooms.mddapi.model.Feed;
+import com.openclassrooms.mddapi.model.dto.SubjectDto;
+import com.openclassrooms.mddapi.service.FeedService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/feed")
+public class FeedController {
+    private FeedService feedService;
+    public FeedController (FeedService feedService){
+        this.feedService = feedService;
+    }
+    @GetMapping("/{id_user}")
+    public List<Feed> getFeedList(){
+        return feedService.findFeedList();
+    }
+
+    @GetMapping("/feed_list")
+    public List<Feed> getFeedSubscriptionList(){
+        return feedService.findFeedList();
+    }
+    @GetMapping("/subscribe_subjectDto_list")
+    public List<SubjectDto>findSubjectListSubscribed(){
+        return feedService.findSubjectListSubscribed();
+    }
 }
