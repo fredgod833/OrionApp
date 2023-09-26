a2f449f1093c82e93562a590f666d19ef3cd60bf
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-09-26 10:04:52 +0200

    :truck: Subject endpoints added

--- back/src/main/java/com/openclassrooms/mddapi/controller/SubjectController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/SubjectController.java
@@ -1,27 +1,35 @@
 package com.openclassrooms.mddapi.controller;
 
 import com.openclassrooms.mddapi.model.Subject;
-import com.openclassrooms.mddapi.repository.SubjectRepository;
+import com.openclassrooms.mddapi.model.dto.SubjectDto;
 import com.openclassrooms.mddapi.service.SubjectService;
-import com.openclassrooms.mddapi.service.SubjectServiceImpl;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
-@RequestMapping(path = "/api/subject")
+@RequestMapping(path = "api/subject")
 public class SubjectController {
-    private SubjectService subjectService;
-
+    private final SubjectService subjectService;
     public SubjectController(SubjectService subjectService){
         this.subjectService = subjectService;
     }
-
-    @GetMapping("/list")
+    @GetMapping(path = "/subject_list")
     public List<Subject> subjectList(){
       return subjectService.getSubjectList();
+    }
+    @GetMapping("/{id_subject}")
+    public Subject getSubjectById(@PathVariable(name = "id_subject") int id_subject){
+        return subjectService.getSubjectById(id_subject);
+    }
+    @GetMapping("/subjectDto_list")
+    public List<SubjectDto> getSubjectDtoList(){
+        return subjectService.findSubjectDtoList();
     }
 
 }
