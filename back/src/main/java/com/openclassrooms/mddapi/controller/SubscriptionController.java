56a5315c771311a0c65c5061df87f221477a334e
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-09-26 10:05:44 +0200

    :truck: Subscription endpoint added

--- back/src/main/java/com/openclassrooms/mddapi/controller/SubscriptionController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/SubscriptionController.java
@@ -1,2 +1,32 @@
-package com.openclassrooms.mddapi.controller;public class SubscriptionController {
+package com.openclassrooms.mddapi.controller;
+
+import com.openclassrooms.mddapi.model.Subscription;
+import com.openclassrooms.mddapi.model.dto.SubjectDto;
+import com.openclassrooms.mddapi.service.SubscriptionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/subscription")
+public class SubscriptionController {
+    private final SubscriptionService subscriptionService;
+
+    public SubscriptionController(SubscriptionService subscriptionService){
+        this.subscriptionService = subscriptionService;
+    }
+
+    @GetMapping("/subscribed_list")
+    public List<SubjectDto> getSubscriptionList(){
+        return subscriptionService.getSubscribedList();
+    }
+
+    @GetMapping("/{subscription_id}")
+    public ResponseEntity<?> getSubscriptionById(@PathVariable("subscription_id") int subscription_id){
+        return subscriptionService.getSubscriptionById(subscription_id);
+    }
 }
