f1bfc557bf48435648f4ee76a5659c1526029606
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-09-29 01:17:49 +0200

    :heavy_plus_sign: Add standAlone component as dependency

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,15 +6,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatMenuModule } from '@angular/material/menu';
+import menuBar from './components/menu.component';
+import Subject from './pages/subject/subject.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    HttpClientModule
+    HttpClientModule,
+    MatMenuModule,
+    menuBar,
+    Subject
   ],
   providers: [],
   bootstrap: [AppComponent],
