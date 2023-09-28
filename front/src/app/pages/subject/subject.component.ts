1f220017915df6b52928dd2dd045a674d8787e34
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-09-29 01:05:30 +0200

    :sparkles: Subject component added

--- front/src/app/pages/subject/subject.component.ts
+++ front/src/app/pages/subject/subject.component.ts
@@ -1,29 +1,34 @@
 import { Component, OnInit } from "@angular/core";
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-import { SubjectService } from "../services/SubjectService";
+import { SubjectService } from "../services/subject.service";
+import { SubjectDto } from "../model/subjectdto";
+import { NgFor } from "@angular/common";
 
 @Component({
     selector: 'app-subject',
     templateUrl: './subject.component.html',
     styleUrls: ['./subject.component.scss'],
     standalone: true,
-    imports: [MatCardModule, MatButtonModule],
+    imports: [MatCardModule, MatButtonModule, NgFor],
 })
-export class Subject implements OnInit{
+export default class Subject implements OnInit{
 
-    longText!:any;
-
+    subject_list: SubjectDto[] = [];
+    
     constructor(private pathService: SubjectService){}
     ngOnInit(){
       this.getSubjectList()
     };
 
     getSubjectList(){
-      this.pathService.getSubjectList().subscribe((response) => {
-        response.map(val => this.longText = val.description)
+      this.pathService.getSubjectList().subscribe(response => {
+        for(let i=0; i < response.length; i++){
+          console.log("BOUCLE: ", response[i]);
+
+          this.subject_list.push(response[i]);
+        }
       })
+
     }
-      
-    
 }
