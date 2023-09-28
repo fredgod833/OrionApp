7dee39c77e36e15b6075fa1483aac9b6afb5d78c
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-09-29 01:16:16 +0200

    Parent routes for children

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { Subject } from './pages/subject/subject.component';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-   {path: 'subject', component: Subject}
+  { path: '', component: HomeComponent,
+  loadChildren: () => import('./pages/menu.router').then((m)=>m.MENU_ROUTER) },
+
   ];
 
 @NgModule({
