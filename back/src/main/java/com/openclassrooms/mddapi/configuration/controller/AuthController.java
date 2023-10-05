352ce9dcf3539c2155e095a62b3940a942c9940b
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-10-05 16:27:46 +0200

    :sparkles: Authentication routes added

--- back/src/main/java/com/openclassrooms/mddapi/configuration/controller/AuthController.java
+++ back/src/main/java/com/openclassrooms/mddapi/configuration/controller/AuthController.java
@@ -1,2 +1,31 @@
-package com.openclassrooms.mddapi.configuration.controller;public class AuthController {
+package com.openclassrooms.mddapi.configuration.controller;
+
+import com.openclassrooms.mddapi.configuration.AuthService;
+import com.openclassrooms.mddapi.configuration.model.LoginRequest;
+import com.openclassrooms.mddapi.configuration.model.Token;
+import com.openclassrooms.mddapi.model.User;
+import com.openclassrooms.mddapi.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+
+@RestController
+@RequestMapping("api/auth")
+public class AuthController {
+    private final AuthService authService;
+    public AuthController(AuthService authService){
+        this.authService = authService;
+    }
+    @PostMapping(path = "/login")
+    public Token login(@RequestBody User user) throws UserPrincipalNotFoundException {
+        return authService.login(user);
+    }
+
+    @PostMapping(path = "/register")
+    public User register(@RequestBody User user){
+        return authService.register(user);
+    }
 }
