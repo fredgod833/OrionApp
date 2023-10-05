19cab357d5509a07846eefc8b645f84455db834d
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-10-05 16:49:18 +0200

    :necktie: Return user by its email method added

--- back/src/main/java/com/openclassrooms/mddapi/service/UserService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/UserService.java
@@ -1,9 +1,12 @@
 package com.openclassrooms.mddapi.service;
 
+import com.openclassrooms.mddapi.model.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
     ResponseEntity<?> user_id(int id_user);
+
+    User getByEmail(String email);
 }
