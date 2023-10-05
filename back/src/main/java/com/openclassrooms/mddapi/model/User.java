a2b7d76d3240e2916baa372cbe5bf0bc0c02b4b1
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-10-05 16:43:46 +0200

    :safety_vest: Property added for User

--- back/src/main/java/com/openclassrooms/mddapi/model/User.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/User.java
@@ -1,19 +1,22 @@
 package com.openclassrooms.mddapi.model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Builder;
 
-import javax.persistence.Id;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Column;
 import javax.persistence.OneToOne;
-import javax.persistence.CascadeType;
 import javax.persistence.FetchType;
+import javax.persistence.CascadeType;
+
 
 @Entity
 @Table(name = "user")
@@ -21,12 +24,14 @@ import javax.persistence.FetchType;
 @Setter
 @AllArgsConstructor
 @RequiredArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id_user;
     private String username;
     private String lastname;
+    private String email;
     private String password;
     @OneToOne(mappedBy = "user",
     fetch = FetchType.EAGER,
