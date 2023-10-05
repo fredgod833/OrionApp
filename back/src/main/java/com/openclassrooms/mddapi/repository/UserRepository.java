021bfeb0fd1c2a0d0725574165220d389ff39ea5
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-10-05 16:47:43 +0200

    :card_file_box: Return user by its email from the database

--- back/src/main/java/com/openclassrooms/mddapi/repository/UserRepository.java
+++ back/src/main/java/com/openclassrooms/mddapi/repository/UserRepository.java
@@ -3,9 +3,13 @@ package com.openclassrooms.mddapi.repository;
 import com.openclassrooms.mddapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @EnableJpaRepositories
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> getByEmail(String email);
 }
