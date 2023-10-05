dfdcfc6c2827ec77100d81111522ed87269da3c6
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-10-05 16:32:37 +0200

    :art: Implements CustomerUserDetailsService layer from spring to manage users authentication

--- back/src/main/java/com/openclassrooms/mddapi/configuration/CustomerUserDetailsService.java
+++ back/src/main/java/com/openclassrooms/mddapi/configuration/CustomerUserDetailsService.java
@@ -1,2 +1,26 @@
-package com.openclassrooms.mddapi.configuration;public class CustomerUserDetailsService {
+package com.openclassrooms.mddapi.configuration;
+
+import com.openclassrooms.mddapi.model.User;
+import com.openclassrooms.mddapi.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    public CustomerUserDetailsService(UserService userService){
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.getByEmail(email);
+        if (user == null){
+            throw new UsernameNotFoundException("USER NOT FOUND");
+        }
+        return new CustomerUserDetails(user);
+    }
 }
