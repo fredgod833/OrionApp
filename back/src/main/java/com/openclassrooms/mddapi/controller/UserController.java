e6e74f9cf779a1bd10b2132e4ef60135fcdd1f13
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-10-05 16:45:27 +0200

    :zap: Improve imports for better perform

--- back/src/main/java/com/openclassrooms/mddapi/controller/UserController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/UserController.java
@@ -2,10 +2,7 @@ package com.openclassrooms.mddapi.controller;
 
 import com.openclassrooms.mddapi.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/user")
@@ -15,7 +12,7 @@ public class UserController {
         this.userService = userService;
     }
     @GetMapping("/{id_user}")
-    public ResponseEntity<?> getUserById(@PathVariable(name = "id_user")int id_user){
+    public ResponseEntity<?> getUserById(@PathVariable(name = "id_user")int id_user) {
         return userService.user_id(id_user);
     }
 }
