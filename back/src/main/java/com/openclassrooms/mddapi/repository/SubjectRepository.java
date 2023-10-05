5ad57ca873cf204e5d8df67032b5b7bf2923ef84
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-10-05 16:37:14 +0200

    :fire: Unused code in SubjectRepository removed

--- back/src/main/java/com/openclassrooms/mddapi/repository/SubjectRepository.java
+++ back/src/main/java/com/openclassrooms/mddapi/repository/SubjectRepository.java
@@ -2,14 +2,8 @@ package com.openclassrooms.mddapi.repository;
 
 import com.openclassrooms.mddapi.model.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, Integer> {
-
-//    @Query(value = "SELECT * FROM post INNER JOIN subject WHERE id_subject=1", nativeQuery = true)
-    List<Subject> findByidSubject(int id_subject);
 }
