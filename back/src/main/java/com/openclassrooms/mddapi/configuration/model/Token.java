869043c3801c1694e6540d04220e289234d31366
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-10-05 16:26:27 +0200

    :closed_lock_with_key: Token object added

--- back/src/main/java/com/openclassrooms/mddapi/configuration/model/Token.java
+++ back/src/main/java/com/openclassrooms/mddapi/configuration/model/Token.java
@@ -1,2 +1,15 @@
-package com.openclassrooms.mddapi.configuration.model;public class Token {
+package com.openclassrooms.mddapi.configuration.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Builder;
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
+public class Token {
+    private String token;
 }
