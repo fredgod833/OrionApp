7b5c959af154b797b3622109332b66e82ce9a0d0
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-10-05 16:33:49 +0200

    :sparkles: Create subject route added

--- back/src/main/java/com/openclassrooms/mddapi/controller/SubjectController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/SubjectController.java
@@ -3,15 +3,10 @@ package com.openclassrooms.mddapi.controller;
 import com.openclassrooms.mddapi.model.Subject;
 import com.openclassrooms.mddapi.model.dto.SubjectDto;
 import com.openclassrooms.mddapi.service.SubjectService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin("*")
 @RestController
 @RequestMapping(path = "api/subject")
 public class SubjectController {
@@ -30,6 +25,11 @@ public class SubjectController {
     @GetMapping("/subjectDto_list")
     public List<SubjectDto> getSubjectDtoList(){
         return subjectService.findSubjectDtoList();
+    }
+
+    @PostMapping("/create_subject")
+    public String createSubject(@RequestBody Subject subject){
+        return subjectService.createSubject(subject);
     }
 
 }
