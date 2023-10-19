82315c8e53ed246c9f992bdabbcf17f9e79ea3d5
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-10-19 11:33:35 +0200

    :construction: Work in progress for route architecture

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import Subject from './pages/subject/subject.component';
+import Post from './pages/post/post.component';
+import menuBar from './components/menu.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthGuard } from './components/guards/auth.guard';
+import PostSelected from './pages/post/comments/selected.component';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
 const routes: Routes = [
-  { path: '', component: HomeComponent,
-  loadChildren: () => import('./pages/menu.router').then((m)=>m.MENU_ROUTER) },
+  { path: '', component: HomeComponent},  
+  {path: 'login', component: LoginComponent},
+  {path: 'post', component: Post},
+  {path: 'menu', component: menuBar},
+  {path: 'comments', component: PostSelected},
+
+  {
+    path: 'menu', 
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./pages/menu.module').then(m => m.MenuModule)}
 
   ];
 
