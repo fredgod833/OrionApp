9d05de46a5e29ffddc0a4b4db768fa49998995aa
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-10-19 12:03:29 +0200

    :necktie: Changing way receiving data for post

--- front/src/app/pages/services/post.service.ts
+++ front/src/app/pages/services/post.service.ts
@@ -2,22 +2,20 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import PostInterface from "../model/post";
+import { Token } from "@angular/compiler";
 
 @Injectable({
     providedIn: 'root'
 })
 export default class postService{
 
-    private pathService = 'api/post'
+    private pathService = 'api/post';
+    private token!: Token;
 
     constructor(private httClient: HttpClient){};
 
-    public getPostList(){
+    public getPostList():Observable<PostInterface[]>{
 
-        const response: Observable<PostInterface[]> = this.httClient.get<PostInterface[]>(`${this.pathService}/post_list`);
-       
-        console.log("Response", response);
-
-        return response;
-    }
+         return this.httClient.get<PostInterface[]>(`${this.pathService}/post_list`);
+        }
 }
