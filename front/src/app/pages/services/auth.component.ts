c56aa330aff5bb6e40e888e733a9e7cff89ab27d
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-10-19 11:21:42 +0200

    :necktie: Service layer for authentication

---
+++ front/src/app/pages/services/auth.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Token } from "../../security/model/token.component";
+import { Observable } from "rxjs";
+import { Injectable } from "@angular/core";
+import LoginRequest from "src/app/interfaces/login.component";
+import User from "src/app/interfaces/user.interface";
+
+@Injectable({
+    providedIn: 'root'
+})
+export default class AuthService{
+
+    private path = 'api/auth';
+
+    constructor(private httpClient: HttpClient ){}
+
+    public login(loginRequest: LoginRequest):Observable<Token>{
+       return this.httpClient.post<Token>(`${this.path}/login`, loginRequest);
+      
+    }
+
+    public me():Observable<User>{
+        return this.httpClient.get<User>(`${this.path}/me`);
+    }
+
+}
