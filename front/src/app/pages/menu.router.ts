e9cc8fa76da9ce51351fa461b2d7bca91265a984
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-10-19 11:56:16 +0200

    :construction: Working in progress for routing app

--- front/src/app/pages/menu.router.ts
+++ front/src/app/pages/menu.router.ts
@@ -1,10 +1,18 @@
-import { Route } from "@angular/router";
+import { Route, RouterModule } from "@angular/router";
 import Subject from "./subject/subject.component";
 import menuBar from "../components/menu.component";
 import Post from "./post/post.component";
+import { LoginComponent } from "./login/login.component";
+import { NgModule } from "@angular/core";
 
-export const MENU_ROUTER:Route[] = [
-    {path: 'subject', component: Subject},
-    {path: 'post', component: Post},
-    {path: 'menu', component: menuBar}
+const MENU_ROUTER:Route[] = [
+    {title: 'Subject', path: 'subject', component: Subject},
+    // {path: 'menu', component: menuBar},
+    {title: 'Post', path:'post', component: Post}
 ];
+
+@NgModule({
+    imports: [RouterModule.forChild(MENU_ROUTER)],
+    exports: [RouterModule]
+})
+export class MenuRoutingModule {}
