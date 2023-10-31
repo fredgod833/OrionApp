ad1b32b2d650920f95eb10208e4353e9de93d3b1
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-10-31 09:23:21 +0100

    :art: Change structure of model

--- back/src/main/java/com/openclassrooms/mddapi/model/User.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/User.java
@@ -1,22 +1,12 @@
 package com.openclassrooms.mddapi.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Builder;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Column;
-import javax.persistence.OneToOne;
-import javax.persistence.FetchType;
-import javax.persistence.CascadeType;
-
+import javax.persistence.*;
 
 @Entity
 @Table(name = "user")
@@ -28,14 +18,11 @@ import javax.persistence.CascadeType;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
     private int id_user;
     private String username;
     private String lastname;
     private String email;
     private String password;
-    @OneToOne(mappedBy = "user",
-    fetch = FetchType.EAGER,
-    cascade = CascadeType.ALL)
-    @JsonManagedReference
-    private Feed feed;
-}
+    @OneToOne
+    Subscription subscription;}
