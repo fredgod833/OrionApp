a7a3c3ed770ae36ef5031bbdace323d62b1c34c9
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-11-23 16:23:09 +0100

    :sparkles: Add comments route for redirections

--- front/src/app/pages/menu.router.ts
+++ front/src/app/pages/menu.router.ts
@@ -4,11 +4,16 @@ import menuBar from "../components/menu.component";
 import Post from "./post/post.component";
 import { LoginComponent } from "./login/login.component";
 import { NgModule } from "@angular/core";
+import PostSelected from "./post/comments/selected.component";
+import CreatePost from "./post/create/post.create";
 
 const MENU_ROUTER:Route[] = [
-    {title: 'Subject', path: 'subject', component: Subject},
+    // {title: 'Subject', path: 'subject', component: Subject},
     // {path: 'menu', component: menuBar},
-    {title: 'Post', path:'post', component: Post}
+    // {title: 'Post', path:'post', component: Post},
+    {title: 'comments', path: 'comments', component: PostSelected},
+    // {path: 'create-post', component: CreatePost},
+
 ];
 
 @NgModule({
