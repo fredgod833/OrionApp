bce5ce8438dc7e3b30cbff668d3f3168df211251
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-11-23 15:32:35 +0100

    :sparkles: Create functions to redirect login | register form

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  constructor(){}
+  constructor(private route: Router){}
   ngOnInit(): void {}
+
+  login(){
+    return this.route.navigate(['/login']);
+  }
+
+  register(){
+    return this.route.navigate(['register']);
+  }
 }
