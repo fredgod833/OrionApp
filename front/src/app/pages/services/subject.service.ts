81521fe8bfc9459ba3db88c5847348556af7846c
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-11-23 14:59:20 +0100

    :heavy_minus_sign: Remove unused dependency

--- front/src/app/pages/services/subject.service.ts
+++ front/src/app/pages/services/subject.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { SubjectDto } from "../model/subjectdto";
 import { Observable } from "rxjs";
 
@@ -11,8 +11,7 @@ export class SubjectService{
     private pathService = 'api/subject';
 
     constructor(private httpClient: HttpClient){};
-    // public token =  localStorage.getItem("token");
-    public getSubjectList():Observable<SubjectDto[]>{
+        public getSubjectList():Observable<SubjectDto[]>{
 
        return this.httpClient.get<SubjectDto[]>(`${this.pathService}/subjectDto_list`);
        
