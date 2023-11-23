90c7572f31884a8ea5f0968d88d5efce6f4bf2ef
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-11-23 15:38:48 +0100

    :sparkles: Create function for post selected

--- front/src/app/pages/post/post.component.ts
+++ front/src/app/pages/post/post.component.ts
@@ -2,38 +2,35 @@ import { Component, OnInit } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import PostInterface from "../model/post";
-import postService from "../services/post.service";
 import { DatePipe, NgFor, NgIf} from "@angular/common";
-import { RouterOutlet } from "@angular/router";
-
+import { RouterModule, RouterOutlet } from "@angular/router";
+import PostSelected from "./comments/selected.component";
+import CreatePost from "./create/post.create";
+import { PostService } from "../services/post.service";
 
 @Component({
-  selector: 'app-post',
-  templateUrl: './post.component.html',
-  styleUrls: ['post.component.scss'],
-  standalone: true,
-  imports: [MatCardModule, MatButtonModule, NgFor, NgIf, RouterOutlet],
-  providers: [DatePipe]
+    selector: 'app-post',
+    templateUrl: './post.component.html',
+    styleUrls: ['post.component.scss'],
+    standalone: true,
+    providers: [DatePipe],
+    imports: [MatCardModule, MatButtonModule, NgFor, NgIf, RouterOutlet, PostSelected, RouterModule, CreatePost]
 })
 export default class Post implements OnInit{
     post_list:PostInterface[] = [];
 
-    private postService;
-    private isActive =  false;
+    public isActive =  false;
 
-    constructor(postService: postService){
-      this.postService = postService;
+    public postList = this.postService.getPostList();
+
+    constructor(private postService: PostService){
     }
 
-    ngOnInit(): void {
-      this.getPost_list()
-    };
-    
-    public getPost_list(){
-      this.postService.getPostList().subscribe((list)=> list.map((posts)=> 
-      this.post_list.push(posts)));
-
-    }
+  ngOnInit(): void {
+    this.postList.subscribe((list: PostInterface[])=> list.map((posts: PostInterface)=>{ 
+      this.post_list.push(posts);
+    }))
+  }
 
     formatPostDate(date: Date){
      const dateFromPost = new Date(date);
@@ -43,7 +40,8 @@ export default class Post implements OnInit{
      return formattedDate;
     }
 
-    isClicked(){
+    isPostSelected(){
       this.isActive = true;
     }
-}
+
+  }
