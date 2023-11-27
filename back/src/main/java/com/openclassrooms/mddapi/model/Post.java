3e1bab4801bd5d83a219b76a2a530cccc86527b7
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-11-27 11:01:42 +0100

    :fire: Remove unused code and dependency

--- back/src/main/java/com/openclassrooms/mddapi/model/Post.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/Post.java
@@ -1,6 +1,5 @@
 package com.openclassrooms.mddapi.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Setter;
 import lombok.Getter;
 import lombok.AllArgsConstructor;
@@ -11,7 +10,6 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
 
 @Entity
@@ -30,7 +28,4 @@ public class Post {
     private String author;
     private String content;
     private String comments;
-    @ManyToOne
-    @JsonBackReference
-    private Subject subject;
 }
