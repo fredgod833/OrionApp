3049233fb69e35cd7aa40da9ee0173206a473c6e
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-11-27 16:52:05 +0100

    :heavy_plus_sign: Add dependency for user template usage

--- front/src/app/pages/user/user.information.ts
+++ front/src/app/pages/user/user.information.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @Component({
     selector: 'user-info',
     templateUrl: './user.information.html',
     styleUrls: ['./user.information.scss'],
-    standalone: true
+    standalone: true,
+    imports: [MatFormFieldModule, MatCardModule]
 })
 export default class UserInformation{}
