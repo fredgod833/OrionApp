cf229de970f30758b1323512bc1d609c9a8e0cdc
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-11-27 15:43:24 +0100

    :sparkles: Create component to get user information profil

---
+++ front/src/app/pages/user/user.information.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'user-info',
+    templateUrl: './user.information.html',
+    styleUrls: ['./user.information.scss'],
+    standalone: true
+})
+export default class UserInformation{}
