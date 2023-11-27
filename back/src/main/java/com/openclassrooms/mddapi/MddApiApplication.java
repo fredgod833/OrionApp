143601d350749fe73011982d88771c4ba82e5ead
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-11-27 11:08:50 +0100

    :closed_lock_with_key: Enable configuration properties for Rsa keys to be loaded

--- back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
+++ back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
@@ -1,7 +1,9 @@
 package com.openclassrooms.mddapi;
 
+import com.openclassrooms.mddapi.configuration.security.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class MddApiApplication {
 
 	public static void main(String[] args) {
