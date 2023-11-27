f14255d31174e51607e7238fa0100cfe1ef72303
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-11-27 10:39:49 +0100

    :sparkles: Route to get current user authenticated

--- back/src/main/java/com/openclassrooms/mddapi/configuration/controller/AuthController.java
+++ back/src/main/java/com/openclassrooms/mddapi/configuration/controller/AuthController.java
@@ -1,16 +1,14 @@
 package com.openclassrooms.mddapi.configuration.controller;
 
 import com.openclassrooms.mddapi.configuration.AuthService;
-import com.openclassrooms.mddapi.configuration.model.LoginRequest;
 import com.openclassrooms.mddapi.configuration.model.Token;
 import com.openclassrooms.mddapi.model.User;
-import com.openclassrooms.mddapi.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.openclassrooms.mddapi.model.dto.UserDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.nio.file.attribute.UserPrincipalNotFoundException;
+
 
 @RestController
 @RequestMapping("api/auth")
@@ -20,12 +18,16 @@ public class AuthController {
         this.authService = authService;
     }
     @PostMapping(path = "/login")
-    public Token login(@RequestBody User user) throws UserPrincipalNotFoundException {
+    public Token login(@RequestBody User user) {
         return authService.login(user);
     }
-
     @PostMapping(path = "/register")
     public User register(@RequestBody User user){
         return authService.register(user);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> getMe(){
+        return new ResponseEntity<>(authService.getMe(), HttpStatus.OK);
     }
 }
