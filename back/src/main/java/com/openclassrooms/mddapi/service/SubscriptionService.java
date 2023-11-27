ff641e61bd413a1a90c7685e8e877ecfb8803f0a
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-11-27 10:47:14 +0100

    :sparkles: Create subscribe and unsubscribe methods

--- back/src/main/java/com/openclassrooms/mddapi/service/SubscriptionService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/SubscriptionService.java
@@ -1,14 +1,22 @@
 package com.openclassrooms.mddapi.service;
 
+import com.openclassrooms.mddapi.model.Subject;
 import com.openclassrooms.mddapi.model.Subscription;
+import com.openclassrooms.mddapi.model.User;
 import com.openclassrooms.mddapi.model.dto.SubjectDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Service
 public interface SubscriptionService {
     List<SubjectDto> getSubscribedList();
     ResponseEntity<?> getSubscriptionById(int subscription_id);
+
+    List<Subscription> subscriptions();
+    Subscription subscribe(HttpServletRequest request, int id);
+    ResponseEntity<Subscription> unsubscribe(HttpServletRequest request, int id);
+
 }
