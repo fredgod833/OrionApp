5b9dce450834aa63fc24d91e56c402568552d346
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-11-27 11:01:02 +0100

    :fire: Remove unused code and dependency

--- back/src/main/java/com/openclassrooms/mddapi/model/Subject.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/Subject.java
@@ -1,7 +1,5 @@
 package com.openclassrooms.mddapi.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.RequiredArgsConstructor;
@@ -16,8 +14,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Column;
 import javax.persistence.OneToMany;
 import javax.persistence.CascadeType;
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,21 +28,11 @@ public class Subject {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_subject")
-    private int idSubject;
+    private int id_subject;
     @Column(name = "title")
     private String title;
     @Column(name = "description")
     private String description;
-    @Column(name = "subscribe")
-    private Boolean isSubscribed;
-    @OneToMany(
-            mappedBy = "subject",
-            cascade = CascadeType.ALL,
-            orphanRemoval = true,
-            fetch = FetchType.EAGER)
-    @JsonManagedReference
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Post> postList = new ArrayList<>();
-    @ManyToOne
-    @JsonBackReference
-    private Subscription subscription;
 }
