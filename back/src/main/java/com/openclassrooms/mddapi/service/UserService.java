d112a6694a07d8aa945ff545298012ea2ce005a2
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-12-01 10:20:08 +0100

    Declare username and email change method to implement

--- back/src/main/java/com/openclassrooms/mddapi/service/UserService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/UserService.java
@@ -2,14 +2,16 @@ package com.openclassrooms.mddapi.service;
 
 import com.openclassrooms.mddapi.model.Post;
 import com.openclassrooms.mddapi.model.User;
-import org.springframework.http.ResponseEntity;
+import com.openclassrooms.mddapi.model.dto.UserDto;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Service
 public interface UserService {
     User getUserById(int id_user);
+
+    User changeUserUsernameAndEmail(UserDto userDto);
+
     User subscribe(int id_user, int id_subject);
     User getByEmail(String email);
     User unsubscribe(int id_user, int id_subject);
