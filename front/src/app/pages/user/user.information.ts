15a559c546783d917105f1e2bebd8b99dec9d6a9
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-12-01 09:38:19 +0100

    :sparkles: New features User informations, change username and password, logout

--- front/src/app/pages/user/user.information.ts
+++ front/src/app/pages/user/user.information.ts
@@ -1,12 +1,78 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { MatCardModule } from "@angular/material/card";
 import { MatFormFieldModule } from "@angular/material/form-field";
+import AuthService from "../services/auth.component";
+import User from "src/app/interfaces/user.interface";
+import { SubjectDto } from "../model/subjectdto";
+import { NgFor } from "@angular/common";
+import { UserService } from "../services/user.service";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'user-info',
     templateUrl: './user.information.html',
     styleUrls: ['./user.information.scss'],
     standalone: true,
-    imports: [MatFormFieldModule, MatCardModule]
+    imports: [MatFormFieldModule, MatCardModule, NgFor, ReactiveFormsModule]
 })
-export default class UserInformation{}
+export default class UserInformation implements OnInit{
+    public subjectList: SubjectDto[] = [];
+    public form_profil!: FormGroup;
+    
+    constructor(private formBuilder:FormBuilder, private authService: AuthService, private userService: UserService, 
+    private router: Router){}
+    
+    ngOnInit(): void {
+        this.initFormProfil();
+        this.userProfil();
+    }
+
+    
+    public userProfil(){
+
+        this.authService.me().subscribe({
+            next:(value)=> {
+                console.log(value.subscription.subjectList);
+                if(value.subscription != null && value.subscription.subjectList.length > 0){
+                    value.subscription.subjectList.map(subscribed => {
+                        this.subjectList.push(subscribed);
+                    });
+                   
+                }
+            },
+        })
+    }
+
+    public changeUserUsernameAndEmail(){
+        console.log(this.form_profil.value)
+        this.authService.me().subscribe({next:(value)=> {
+
+            const usernameAndEmail  = this.form_profil.value as User;
+            value.username = usernameAndEmail.username;
+            value.email = usernameAndEmail.email;
+
+            this.userService.changeUserUsernameAndEmail(value).subscribe({
+                next(){
+                    console.log("Username and Email changed: ", value);
+                }
+            })
+        },});
+    }
+
+    public logOut(){
+        this.authService.logout().subscribe({
+            next:()=> {
+                console.log("Logout sucessfull");
+                this.router.navigate(['/']);
+            },
+        });
+    }
+
+    public initFormProfil(){
+        this.form_profil = this.formBuilder.group({
+            username: ["", [Validators.required]],
+            email: ["", [Validators.required]]
+        })
+    }
+}
