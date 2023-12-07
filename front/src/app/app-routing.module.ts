b912c99f0add3dd8fc161b8be57259e674baad3c
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-12-07 09:46:32 +0100

    :truck: Update Routes directions

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,28 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import Subject from './pages/subject/subject.component';
-import Post from './pages/post/post.component';
 import menuBar from './components/menu.component';
 import { LoginComponent } from './pages/login/login.component';
-import { AuthGuard } from './components/guards/auth.guard';
 import PostSelected from './pages/post/comments/selected.component';
-import CreatePost from './pages/post/create/post.create';
+import Register from './pages/register/register.component';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
 const routes: Routes = [
   { path: '', component: HomeComponent},  
   {path: 'login', component: LoginComponent},
-  {path: 'post', component: Post},
   {path: 'menu', component: menuBar},
   {path: 'comments', component: PostSelected},
-  {path: 'create-post', component: CreatePost},
+  {path: 'inscription', component: Register},
 
-  {
-    path: 'menu', 
-    canActivate: [AuthGuard],
-    loadChildren: () => import('./pages/menu.router').then(m => m.MenuRoutingModule)}
 
   ];
 
