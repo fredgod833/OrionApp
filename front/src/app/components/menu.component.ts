3a13d0ea9b4828d6c0d3a008f8a0d6664d0edb47
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-12-07 09:39:17 +0100

    :bug: Conflit of pages

--- front/src/app/components/menu.component.ts
+++ front/src/app/components/menu.component.ts
@@ -1,22 +1,21 @@
 import { Component } from "@angular/core";
-import { Router, RouterModule } from "@angular/router";
-import { SessionService } from "../services/session.service";
+import { RouterModule } from "@angular/router";
 import Subject from "../pages/subject/subject.component";
 import Post from "../pages/post/post.component";
 import { NgClass, NgIf } from "@angular/common";
 import CreatePost from "../pages/post/create/post.create";
 import ButtonCreation from "../pages/post/button/button.post.create";
 import UserInformation from "../pages/user/user.information";
-import AuthService from "../pages/services/auth.component";
+
 
 
 @Component({
     selector: 'app-menu',
     templateUrl: './menu.component.html',
-    styleUrls:['./menu.component.scss'],
-    standalone:true,
-    imports: [RouterModule, Subject, Post, NgIf, CreatePost, ButtonCreation, NgClass, UserInformation],
-  })
+    styleUrls: ['./menu.component.scss'],
+    standalone: true,
+    imports: [RouterModule, Subject, Post, NgIf, CreatePost, ButtonCreation, NgClass, UserInformation]
+})
 
 export default class menuBar{
 
@@ -26,25 +25,36 @@ export default class menuBar{
   isButtonCreationActive = false;
   isUserInformationActive = false;
 
-  constructor(private sessionService: SessionService, private authService: AuthService, private router: Router){}
+  constructor(){}
+  
 
+  //Enable post page and Unable others
   activePostLink(){
-    this.sessionService.isLogged;
     this.isPostLinkActive = true;
     this.isSubjectLinkActive = false;
-
+    this.isUserInformationActive = false;
+    this.isPostCreationActive = false;
   }
+
+  //Enable subject page and Unable others
   activeSubjectLink(){
     this.isSubjectLinkActive = true;
     this.isPostLinkActive = false;
+    this.isUserInformationActive = false;
+    this.isPostCreationActive = false;
   }
-
+  //Enable post creation page and Unable other
   activePostCreation(){
     this.isPostCreationActive = true;
     this.isPostLinkActive = false;
+    this.isUserInformationActive = false;
+    this.isSubjectLinkActive = false;
   }
 
   activeUserInformation(){
+    this.isSubjectLinkActive = false;
+    this.isPostLinkActive = false;
+    this.isPostCreationActive = false;
     this.isUserInformationActive = true;
   }
 
