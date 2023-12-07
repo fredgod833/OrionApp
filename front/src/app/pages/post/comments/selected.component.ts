518c7e8a08bb90e60ae86be8c29d8e932193bc4c
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-12-07 09:20:36 +0100

    :heavy_plus_sign: Add dependencies to selected post

--- front/src/app/pages/post/comments/selected.component.ts
+++ front/src/app/pages/post/comments/selected.component.ts
@@ -1,25 +1,26 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormsModule } from "@angular/forms";
-import { MatFormFieldControl, MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatSelectModule } from "@angular/material/select";
-import { ActivatedRoute, RouterModule } from "@angular/router";
+import { ActivatedRoute, Router, RouterModule } from "@angular/router";
 import PostInterface from "../../model/post";
 import { UserService } from "../../services/user.service";
+import menuBar from "src/app/components/menu.component";
 
 @Component({
     selector: 'app-post-selected',
     templateUrl:'./selected.component.html',
     styleUrls: ['./selected.component.scss'],
     standalone: true,
-    imports: [FormsModule, MatFormFieldModule, MatInputModule, RouterModule, MatSelectModule],
+    imports: [FormsModule, MatFormFieldModule, MatInputModule, RouterModule, MatSelectModule, menuBar],
 })
 export default class PostSelected implements OnInit{
 
     public post!:any;
     public comment_text = "";
 
-    constructor(private router:ActivatedRoute, private userService: UserService){}
+    constructor(private router:ActivatedRoute, private userService: UserService, private navigate: Router){}
 
     ngOnInit(): void {
         this.router.params.subscribe(params => {
@@ -41,5 +42,9 @@ export default class PostSelected implements OnInit{
                 return "Post commentered !!!";
             },
         })
+    }
+
+    navigateMenu(){
+        return this.navigate.navigate(['menu'])
     }
 }
