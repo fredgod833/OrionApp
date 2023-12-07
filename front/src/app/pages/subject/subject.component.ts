687f5846a58b37528b38016ff694a4f898623b10
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-12-07 09:02:43 +0100

    :art: Update subscribe function

--- front/src/app/pages/subject/subject.component.ts
+++ front/src/app/pages/subject/subject.component.ts
@@ -3,22 +3,28 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { SubjectService } from "../services/subject.service";
 import { SubjectDto } from "../model/subjectdto";
-import { NgFor } from "@angular/common";
+import { NgFor, NgIf } from "@angular/common";
 import { UserService } from "../services/user.service";
 import AuthService from "../services/auth.component";
+import { forEach } from "cypress/types/lodash";
+import Item from "./item.interface";
 
 @Component({
     selector: 'app-subject',
     templateUrl: './subject.component.html',
     styleUrls: ['./subject.component.scss'],
     standalone: true,
-    imports: [MatCardModule, MatButtonModule, NgFor],
+    imports: [MatCardModule, MatButtonModule, NgFor, NgIf],
 })
 export default class Subject implements OnInit{
 
     subject_list: SubjectDto[] = [];
-    
+    isSubscribed =  false;
+
+    buttonControl:Item[]=[];
+
     constructor(private pathService: SubjectService, private userService: UserService, private authService:AuthService){}
+    
     ngOnInit(){
       this.getSubjectList()
     };
@@ -28,14 +34,28 @@ export default class Subject implements OnInit{
         for(let i=0; i < response.length; i++){
 
           this.subject_list.push(response[i]);
+          console.log("Subject list: ",this.subject_list);
         }
       })
 
     }
 
     subscribe(idSubject: number){
+
+      if(idSubject != 0){
+       // Subscribe subject
+       this.userService.subscribe(idSubject);
+        
+       //Loop to find the right subject
+       this.subject_list.map(val => {
+
+        //Set subscribed to true to the right subject
+        if(idSubject == val.idSubject){
+         val.isSubscribed = true;
+        
+        }
       
-     return this.userService.subscribe(idSubject);
-          
+      })
+        }
     }
 }
