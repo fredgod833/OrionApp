d0d19f16274842a57e8f564c3fe60c4c6cb43342
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-12-11 10:46:44 +0100

    :card_file_box: Add script sql

--- resources/sql/script.sql
+++ resources/sql/script.sql
@@ -1,100 +1,184 @@
--- -----------------------------------------------------
--- `subject`
--- -----------------------------------------------------
-
-CREATE TABLE `subject` (
-  `id_subject` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(20) NULL,
-  `description` VARCHAR(255) NULL,
-  PRIMARY KEY (`id_subject`));
-
-
--- -----------------------------------------------------
--- `subscription`
--- -----------------------------------------------------
-
-CREATE TABLE `subscription` (
-  `id_subscription` INT NOT NULL AUTO_INCREMENT,
-  `subject_id_subject` INT NOT NULL,
-  PRIMARY KEY (`id_subscription`));
-
--- -----------------------------------------------------
--- Table `feed`
--- -----------------------------------------------------
-
-CREATE TABLE `feed` (
-  `id_feed` INT NOT NULL AUTO_INCREMENT,
-  `subscription_id_subscription` INT NOT NULL,
-  PRIMARY KEY (`id_feed`));
-
--- -----------------------------------------------------
--- Table `user`
--- -----------------------------------------------------
-
-CREATE TABLE `user` (
-  `id_user` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(20) NOT NULL,
-  `lastName` VARCHAR(20) NOT NULL,
-  `email` VARCHAR(20) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `feed_id_feed` INT NOT NULL,
-  PRIMARY KEY (`id_user`, `feed_id_feed`));
-
--- -----------------------------------------------------
--- Table `post`
--- -----------------------------------------------------
-
-CREATE TABLE `post` (
-  `id_post` INT NOT NULL,
-  `title` VARCHAR(20) NULL,
-  `date` DATETIME NULL,
-  `author` VARCHAR(15) NULL,
-  `content` TEXT(500) NULL,
-  `comments` VARCHAR(45) NULL,
-  `subject_id_subject` INT NOT NULL,
-  PRIMARY KEY (`id_post`, `subject_id_subject`));
-  
-
--- -----------------------------------------------------
--- INSERT DATA
--- -----------------------------------------------------
-
+CREATE DATABASE  IF NOT EXISTS `mdd_test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `mdd_test`;
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mdd_test
+-- ------------------------------------------------------
+-- Server version	8.0.32
 
-INSERT INTO `subject`(`id_subject`, `title`, `description`)
-VALUES(1, 'JavaScript', "JavaScript est un langage de programmation essentiel pour le développement web moderne. Il permet de créer des expériences web interactives et dynamiques, de concevoir des applications web complexes");
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-INSERT INTO `subscription`(`id_subscription`, `subject_id_subject`)
-VALUES(1, 1);
+--
+-- Table structure for table `post`
+--
 
-INSERT INTO `feed`(`id_feed`, `subscription_id_subscription`)
-VALUES(1, 1);
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post` (
+  `id_post` int NOT NULL AUTO_INCREMENT,
+  `author` varchar(255) DEFAULT NULL,
+  `comments` varchar(255) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_post`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO `post`(`id_post`, `title`, `date`, `author`, `content`, `comments`, `subject_id_subject`)
-VALUES(1, 'Les avantages de JavaScript', '2023-09-08T15:17:00', 'Marvin', "Les avantages de JavaScript sont nombreux et font de ce langage de programmation un élément incontournable dans le monde du développement web. Voici quelques-uns des principaux avantages de JavaScript :
+--
+-- Dumping data for table `post`
+--
 
-Interactivité en temps réel : JavaScript permet de créer des pages web interactives où les éléments de la page peuvent réagir en temps réel aux actions de l'utilisateur. Cela crée des expériences utilisateur plus dynamiques et engageantes.
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+INSERT INTO `post` VALUES (1,'Marvin','Hello World','lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled... ','2023-09-22 12:07:00','Post testing for subject 1'),(2,'Marvin','A comments to say no comments','lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled... ','2023-09-22 12:07:00','Post 2 testing for subject 1'),(3,'',NULL,'sdsdsd','2023-11-20 16:02:39','sdsdsd'),(4,NULL,NULL,NULL,NULL,NULL),(5,NULL,NULL,NULL,NULL,NULL),(6,NULL,NULL,NULL,NULL,NULL),(7,NULL,NULL,NULL,NULL,NULL),(8,NULL,NULL,NULL,NULL,NULL),(9,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
 
-Exécution côté client : JavaScript s'exécute côté client, ce qui signifie que les calculs et les interactions sont traités par le navigateur de l'utilisateur, allégeant ainsi la charge du serveur et améliorant la réactivité des applications.
+--
+-- Table structure for table `subject`
+--
 
-Manipulation du DOM : JavaScript permet de manipuler le Document Object Model (DOM), ce qui facilite l'ajout, la suppression et la modification d'éléments HTML et de styles sur une page web sans avoir à recharger la page.
+DROP TABLE IF EXISTS `subject`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `subject` (
+  `id_subject` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_subject`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-Bibliothèques et frameworks : Il existe de nombreuses bibliothèques et frameworks JavaScript populaires, tels que React, Angular et Vue.js, qui simplifient le développement d'applications web complexes en offrant des composants réutilisables et une structure organisée.
+--
+-- Dumping data for table `subject`
+--
 
-Grande communauté et support : JavaScript bénéficie d'une immense communauté de développeurs, de forums de discussion actifs et de ressources en ligne abondantes. Cela facilite l'apprentissage et la résolution de problèmes.
+LOCK TABLES `subject` WRITE;
+/*!40000 ALTER TABLE `subject` DISABLE KEYS */;
+INSERT INTO `subject` VALUES (1,'This is a subject creation testing  ','Testing '),(2,'This is a subject creation testing  2','Testing 2 '),(3,'This is a subject creation testing  3','Testing 3 ');
+/*!40000 ALTER TABLE `subject` ENABLE KEYS */;
+UNLOCK TABLES;
 
-Compatibilité multiplateforme : JavaScript est pris en charge par tous les principaux navigateurs web, garantissant une compatibilité élevée et une expérience utilisateur cohérente.
+--
+-- Table structure for table `subject_post_list`
+--
 
-Applications côté serveur : Grâce à Node.js, JavaScript peut être utilisé côté serveur pour créer des applications web, des serveurs de chat, des API RESTful, etc., offrant ainsi une stack de développement complète.
+DROP TABLE IF EXISTS `subject_post_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `subject_post_list` (
+  `subject_id_subject` int NOT NULL,
+  `post_list_id_post` int NOT NULL,
+  UNIQUE KEY `UK_oeceiepgah8om284ciqt4efcg` (`post_list_id_post`),
+  KEY `FK3h3fabyufmoib7v0ekm0k6r4p` (`subject_id_subject`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-Évolutivité : Les normes telles qu'ECMAScript (ES6 et versions ultérieures) apportent de nouvelles fonctionnalités et des améliorations de la syntaxe, permettant aux développeurs de créer un code plus propre, plus modulaire et plus maintenable.
+--
+-- Dumping data for table `subject_post_list`
+--
 
-Rapidité d'exécution : Les navigateurs modernes ont des moteurs JavaScript très performants qui rendent l'exécution du code JavaScript rapide et efficace, ce qui est essentiel pour des applications web réactives.
+LOCK TABLES `subject_post_list` WRITE;
+/*!40000 ALTER TABLE `subject_post_list` DISABLE KEYS */;
+INSERT INTO `subject_post_list` VALUES (1,2),(1,1),(1,3);
+/*!40000 ALTER TABLE `subject_post_list` ENABLE KEYS */;
+UNLOCK TABLES;
 
-Intégration avec d'autres technologies : JavaScript peut être facilement intégré à d'autres technologies web, telles que HTML, CSS, AJAX, et des bases de données, pour créer des applications web complètes.
+--
+-- Table structure for table `subscription`
+--
 
-En résumé, JavaScript est un langage de programmation polyvalent et puissant qui offre une multitude d'avantages pour le développement web. Que ce soit pour créer des sites web interactifs, des applications web complexes, ou des applications côté serveur, JavaScript reste un choix incontournable pour les développeurs du monde entier. Sa popularité continue de croître, ce qui en fait un atout précieux pour ceux qui souhaitent s'aventurer dans le monde du développement web.",'',1);
+DROP TABLE IF EXISTS `subscription`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `subscription` (
+  `id_subscription` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id_subscription`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `subscription`
+--
 
-INSERT INTO `user`(`id_user`, `username`, `lastName`, `email`, `password`, `feed_id_feed`)
-VALUES(1, 'test', 'TEST', 'test@email.com', 'test!1234',1);
+LOCK TABLES `subscription` WRITE;
+/*!40000 ALTER TABLE `subscription` DISABLE KEYS */;
+INSERT INTO `subscription` VALUES (1);
+/*!40000 ALTER TABLE `subscription` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subscription_subject_list`
+--
+
+DROP TABLE IF EXISTS `subscription_subject_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `subscription_subject_list` (
+  `subscription_id_subscription` int NOT NULL,
+  `subject_list_id_subject` int NOT NULL,
+  UNIQUE KEY `UK_i67wyams3devmmih6aek8kyv1` (`subject_list_id_subject`),
+  KEY `FKkkmkhwmdpqtgt7ymk4c80ud6n` (`subscription_id_subscription`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subscription_subject_list`
+--
+
+LOCK TABLES `subscription_subject_list` WRITE;
+/*!40000 ALTER TABLE `subscription_subject_list` DISABLE KEYS */;
+INSERT INTO `subscription_subject_list` VALUES (1,3),(1,1),(1,2);
+/*!40000 ALTER TABLE `subscription_subject_list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `subscription_id_subscription` int DEFAULT NULL,
+  PRIMARY KEY (`id_user`),
+  KEY `FKnkpa3m9n0txbh25kbk59nc0dd` (`subscription_id_subscription`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'frida@kalo.com','KALO','$2a$10$UJnnj2f2ZVE8lRZ6Pu6KLu9rb4eCRxP1/h1vHN3KRURTVpkESLNBe','Frida',1),(2,'naldo@castro.com','Cesar',NULL,'Naldo',NULL),(3,'james@brow.com',NULL,NULL,'James',NULL),(4,'test@test.com',NULL,'$2a$10$g439g2oVmP1xxuaoEroU1ucB4rFP.9j/VCcFTCOdJjg.SKrolsv5K','Test',NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-11 10:42:57
