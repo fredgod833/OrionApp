9d8214fe9d20101299dbfd1cf3674e55b61912b9
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-12-17 15:01:07 +0100

    :heavy_minus_sign: Remove unused dependencies

--- front/src/app/pages/services/auth.component.ts
+++ front/src/app/pages/services/auth.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders, HttpInterceptor } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Token } from "../../security/interfaces/token.component";
 import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
