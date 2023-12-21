b7568b8b3b2d756fc7db5560c8dd329500ce9727
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-12-21 12:12:51 +0100

    :art: Add routes path to allow redirections

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import menuBar from './components/menu.component';
 import { LoginComponent } from './pages/login/login.component';
 import PostSelected from './pages/post/comments/selected.component';
 import Register from './pages/register/register.component';
+import CreatePost from './pages/post/create/post.create';
+import Post from './pages/post/post.component';
+import Subject from './pages/subject/subject.component';
+import UserInformation from './pages/user/user.information';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
@@ -14,6 +18,10 @@ const routes: Routes = [
   {path: 'menu', component: menuBar},
   {path: 'comments', component: PostSelected},
   {path: 'inscription', component: Register},
+  {path: 'post-creation', component: CreatePost},
+  {path: 'post', component: Post},
+  {path: 'subject', component: Subject},
+  {path: 'user-information', component: UserInformation}
   ];
 
 @NgModule({
