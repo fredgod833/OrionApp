ab519047f0461f836dbdbe24f482d38aaf1e51c1
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2024-01-02 16:57:46 +0100

    :sparkles: Create method in the interface

--- back/src/main/java/com/openclassrooms/mddapi/service/UserService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/UserService.java
@@ -1,9 +1,12 @@
 package com.openclassrooms.mddapi.service;
 
+import com.openclassrooms.mddapi.model.Comments;
 import com.openclassrooms.mddapi.model.Post;
 import com.openclassrooms.mddapi.model.User;
 import com.openclassrooms.mddapi.model.dto.UserDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Interface to implement
@@ -16,5 +19,9 @@ public interface UserService {
     User getByEmail(String email);
     User unsubscribe(int id_user, int id_subject);
     User deleteUserAccount(int id_user);
-    Post commentPost(Post post);
+    Post commentPost(Post post, Comments comment);
+
+    //TODO: This is a test
+    Comments newComments(Comments comments, int id_post);
+    List<Comments> getCommentsList();
 }
