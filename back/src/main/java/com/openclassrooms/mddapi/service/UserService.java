6163a51d94d8ac264d98f936bc7ad045144a615d
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2024-01-04 12:01:49 +0100

    :art: Update parameters type to be implemented

--- back/src/main/java/com/openclassrooms/mddapi/service/UserService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/UserService.java
@@ -2,6 +2,7 @@ package com.openclassrooms.mddapi.service;
 
 import com.openclassrooms.mddapi.model.Comments;
 import com.openclassrooms.mddapi.model.Post;
+import com.openclassrooms.mddapi.model.Subject;
 import com.openclassrooms.mddapi.model.User;
 import com.openclassrooms.mddapi.model.dto.UserDto;
 import org.springframework.stereotype.Service;
@@ -15,9 +16,9 @@ import java.util.List;
 public interface UserService {
     User getUserById(int id_user);
     User changeUserUsernameAndEmail(UserDto userDto);
-    User subscribe(int id_user, int id_subject);
+    User subscribe(int id_user, Subject subject);
     User getByEmail(String email);
-    User unsubscribe(int id_user, int id_subject);
+    User unsubscribe(int id_user, Subject subject);
     User deleteUserAccount(int id_user);
     Post commentPost(Post post, Comments comment);
 
