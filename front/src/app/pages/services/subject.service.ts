45ac4519c12c62a96a4dc1cf488c027deb2ab1a8
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2024-01-04 11:46:48 +0100

    :sparkles: Add funtion to get all subject data

--- front/src/app/pages/services/subject.service.ts
+++ front/src/app/pages/services/subject.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { SubjectDto } from "../model/subjectdto";
 import { Observable } from "rxjs";
+import Subject from "../model/subject";
 
 @Injectable({
     providedIn: 'root'
@@ -12,9 +13,20 @@ export class SubjectService{
 
     constructor(private httpClient: HttpClient){};
 
-    //Return list of subject
-    public getSubjectList():Observable<SubjectDto[]>{
+    //Return list of subjectDto
+    public getSubjectDtoList():Observable<SubjectDto[]>{
 
        return this.httpClient.get<SubjectDto[]>(`${this.pathService}/subjectDto_list`);   
     }
+
+    //Return a list of subject
+    public getSubjectList():Observable<Subject[]>{
+        const result = this.httpClient.get<Subject[]>(`${this.pathService}/subject_list`)
+        result.subscribe({next(value) {
+            console.log("RESULT",value);
+
+        },})
+        return result;
+    }
+
 }
