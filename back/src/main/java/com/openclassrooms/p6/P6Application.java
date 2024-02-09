59225bfd33ec9cb46ca2dd5552a7cd1ec31bbe06
Younes Lahouiti <loller763@gmail.com>
2024-02-09 10:18:42 +0100

    Re-initialization of the app's code to use the latest version of Java-Spring and the latest version of Angular

---
+++ back/src/main/java/com/openclassrooms/p6/P6Application.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.p6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P6Application.class, args);
+	}
+
+}
