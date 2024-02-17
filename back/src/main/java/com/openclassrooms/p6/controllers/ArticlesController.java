3a273a7dc451569a633d13e214ca38569c489115
Younes Lahouiti <loller763@gmail.com>
2024-02-17 15:27:34 +0100

    Removal of useless imports

--- back/src/main/java/com/openclassrooms/p6/controllers/ArticlesController.java
+++ back/src/main/java/com/openclassrooms/p6/controllers/ArticlesController.java
@@ -37,7 +37,6 @@ import com.openclassrooms.p6.utils.JwtUtil;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.PostMapping;
