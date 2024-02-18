a3045a7e155e39e957522e0ace0df34982bfb16c
Younes Lahouiti <loller763@gmail.com>
2024-02-18 14:03:18 +0100

    Configuration of the router system and addition of the of a 404 page + redirection to the /home page

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,53 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    title: 'Monde de Dév',
+    component: HomeComponent,
+  },
+  /*
+  {
+    path: 'register',
+    title: 'Monde de Dév',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    title: 'Monde de Dév',
+    component: HomeComponent,
+  },
+  {
+    path: 'articles',
+    title: 'Monde de Dév',
+    component: HomeComponent,
+  },
+  {
+    path: 'article/:id',
+    title: 'Monde de Dév',
+    component: HomeComponent,
+  },
+  {
+    path: 'topics',
+    title: 'Monde de Dév',
+    component: HomeComponent,
+  },
+  {
+    path: 'user',
+    title: 'Monde de Dév',
+    component: HomeComponent,
+  },
+  */
+  {
+    path: '**',
+    title: 'Error 404, page not found!',
+    component: NotFoundComponent,
+  },
+];
