826e33f633872aac5a4007bf2c6b4acf0f37b162
Younes Lahouiti <loller763@gmail.com>
2024-02-23 11:59:18 +0100

    fix: Hide header on /home page using router event observables

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/common/header/header.component';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,20 @@ import { HeaderComponent } from './components/common/header/header.component';
 })
 export class AppComponent {
   title = 'mdd-app';
+
+  showHeader = true;
+
+  // * Dependency injections
+  public router = inject(Router);
+
+  // * Observables
+  private navigationEndEvents$ = this.router.events.pipe(
+    filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+  );
+
+  ngOnInit() {
+    this.navigationEndEvents$.subscribe(() => {
+      this.showHeader = this.router.url !== '/home';
+    });
+  }
 }
