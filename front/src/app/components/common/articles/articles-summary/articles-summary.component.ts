d606ed233d84a2d45cf133ab275c717ea6be32dd
Younes Lahouiti <loller763@gmail.com>
2024-02-24 11:49:51 +0100

    Attempt to integrate the articles using signals

--- front/src/app/components/common/articles/articles-summary/articles-summary.component.ts
+++ front/src/app/components/common/articles/articles-summary/articles-summary.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-articles-summary',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './articles-summary.component.html',
-  styleUrl: './articles-summary.component.scss'
+  styleUrl: './articles-summary.component.scss',
 })
 export class ArticlesSummaryComponent {
+  // * Signal  inputs
+  public id = input.required<number>();
 
+  public title = input.required<string>();
+
+  public description = input.required<string>();
+
+  public author = input.required<string>();
+
+  public date = input.required<string>();
+
+  public publishDate = computed(() => {
+    const date = new Date(this.date()).toLocaleDateString();
+
+    return date;
+  });
 }
--- front/src/app/pages/articles/articles.component.ts
+++ front/src/app/pages/articles/articles.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Article } from '@core/types/article.type';
+import { ArticlesSummaryComponent } from '@components/common/articles/articles-summary/articles-summary.component';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-articles',
   standalone: true,
-  imports: [RouterLink],
   templateUrl: './articles.component.html',
   styleUrl: './articles.component.scss',
+  imports: [RouterLink, ArticlesSummaryComponent],
 })
-export class ArticlesComponent {}
+export class ArticlesComponent {
+  public isAscending: boolean = true;
+
+  // Array of articles
+  public arrOfArticles: Array<Article> = [
+    {
+      id: 1,
+      title: 'Article 1',
+      date: '2024-02-23',
+      username: 'user1',
+      description: 'Description of article 1',
+    },
+    {
+      id: 2,
+      title: 'Article 2',
+      date: '2024-02-24',
+      username: 'user2',
+      description: 'Description of article 2',
+    },
+  ];
+
+  // Create a Subject of type Article array
+  public test = new BehaviorSubject<Article[]>(this.arrOfArticles)
+    .asObservable()
+    .subscribe();
+}
