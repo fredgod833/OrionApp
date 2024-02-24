58d77353471b1165e7ee2144e20b349ffbbf1723
Younes Lahouiti <loller763@gmail.com>
2024-02-24 10:38:29 +0100

    Beginning of the implementation of the articles page

--- front/src/app/pages/articles/articles.component.ts
+++ front/src/app/pages/articles/articles.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-articles',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './articles.component.html',
   styleUrl: './articles.component.scss',
 })
