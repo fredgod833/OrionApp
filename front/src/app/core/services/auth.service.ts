8b31b5146f3de1b8d5b146a5041368907394b5e7
Younes Lahouiti <loller763@gmail.com>
2024-02-25 12:13:40 +0100

    Removal of a useless service & addition of the auth service

---
+++ front/src/app/core/services/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+import { WebStorage } from '@lephenix47/webstorage-utility';
+
+// import { CookieService } from '@lephenix47/cookies-utility';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor() {}
+}
