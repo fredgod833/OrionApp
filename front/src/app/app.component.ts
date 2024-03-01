c5eb0dc17bfe087211bab800bec785af8462d733
Younes Lahouiti <loller763@gmail.com>
2024-03-01 09:58:07 +0100

    Beginning fixing an issue where the user info would reset to the initial state when we reload the page

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component, inject } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/common/layout/header/header.component';
 import { filter } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { UserBasicInfo } from '@core/types/user.type';
+import { UserService } from '@core/services/user/user.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +15,11 @@ import { filter } from 'rxjs';
   imports: [RouterOutlet, HeaderComponent],
 })
 export class AppComponent {
-  title = 'mdd-app';
+  private store = inject(Store);
+
+  public userService = inject(UserService);
+
+  public userInfo = toSignal<UserBasicInfo>(this.store.select('userInfo'));
 
   // * Instance properties
   public showHeader = true;
@@ -27,8 +35,11 @@ export class AppComponent {
   );
 
   ngOnInit() {
+    // this.userService.updateUser
+
     this.navigationEndEvents$.subscribe(() => {
-      this.showHeader = this.router.url !== '/home';
+      this.showHeader =
+        this.router.url !== '/home' || Boolean(this.userInfo()?.id);
     });
   }
 }
