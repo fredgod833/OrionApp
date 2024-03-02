241598897fd5aacf5d30e19a062f4a6847995fa7
Younes Lahouiti <loller763@gmail.com>
2024-03-02 10:44:40 +0100

    Addition of the logic to sub and unsub (BUGGY)

--- front/src/app/pages/topics/topics.component.ts
+++ front/src/app/pages/topics/topics.component.ts
@@ -3,7 +3,8 @@ import { Topic, TopicSubscription } from '@core/types/topic.type';
 import { TopicSubscriptionComponent } from '@components/common/topics/topic-subscription/topic-subscription.component';
 import { TopicsContainerComponent } from '@components/shared/topics-container/topics-container.component';
 import { TopicService } from '@core/services/topic/topic.service';
-import { Observable, combineLatest, forkJoin, map } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-topics',
@@ -17,32 +18,62 @@ export class TopicsComponent {
 
   public topicsArray = signal<Topic[]>([]);
 
+  public isLoading = toSignal<boolean>(this.topicService.isLoading$);
+
+  public hasError = toSignal<boolean>(this.topicService.hasError$);
+
+  public errorMessage = toSignal<string>(this.topicService.errorMessage$);
+
   constructor() {
     this.updateUserThemeSubscription =
       this.updateUserThemeSubscription.bind(this);
   }
 
   ngOnInit() {
-    this.topicService.getAllThemesWithSubscription().subscribe((v) => {
+    this.topicService.getAllThemesWithSubscription().subscribe((v: Topic[]) => {
       this.topicsArray.update(() => {
         return v;
       });
-      console.log(this.topicsArray());
     });
   }
 
-  updateUserThemeSubscription(id?: number) {
-    console.log('PARENT Clicked subs toggle btn', this);
+  updateUserThemeSubscription(id: number): void {
+    const topic = this.getTopicById(id) as Topic;
+    console.log('PARENT Clicked subs toggle btn', topic);
 
-    this.topicsArray.update((topics) => {
-      for (let i = 0; i < topics.length; i++) {
-        const topic = topics[i];
+    const { isSubscribed } = topic;
 
-        if (topic.id !== id) {
-          continue;
-        }
+    let subscriber: Subscription;
 
-        topics[i].isSubscribed = !topic.isSubscribed;
+    if (isSubscribed) {
+      subscriber = this.topicService
+        .unsubscribeToTheme(id)
+        .subscribe((value) => {
+          this.updateTopicsArray(id);
+        });
+    } else {
+      subscriber = this.topicService.subscribeToTheme(id).subscribe((value) => {
+        this.updateTopicsArray(id);
+      });
+    }
+  }
+
+  private getTopicById(id: number): Topic | null {
+    const topicsArray = this.topicsArray();
+
+    const topic: Topic | null =
+      topicsArray.find((topic) => {
+        return topic.id === id;
+      }) || null;
+
+    return topic;
+  }
+
+  private updateTopicsArray(id: number): void {
+    this.topicsArray.update((topics: Topic[]) => {
+      const index: number = topics.findIndex((topic: Topic) => topic.id === id);
+      if (index !== -1) {
+        topics[index].isSubscribed = !topics[index].isSubscribed;
       }
 
       return topics;
