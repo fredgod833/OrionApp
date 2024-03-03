aef74f7b56a12257447885eed44a364ed872328f
Younes Lahouiti <loller763@gmail.com>
2024-03-03 11:00:35 +0100

    Addition of the JSDoc to the interceptor

--- front/src/app/core/interceptors/token.interceptor.ts
+++ front/src/app/core/interceptors/token.interceptor.ts
@@ -3,6 +3,12 @@ import { inject } from '@angular/core';
 import { CookiesService } from '@core/services/cookies/cookies.service';
 import { CookieType } from '@lephenix47/cookies-utility';
 
+/**
+ * Interceptor function to attach JWT token to outgoing HTTP requests if present in the cookies
+ * @param req The HTTP request object.
+ * @param next The HTTP handler for the request.
+ * @returns An observable of the HTTP event.
+ */
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const cookiesService = inject(CookiesService);
 
