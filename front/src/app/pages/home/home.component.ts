d8e94d48810ad203225bacf41f92b01a271f1f58
Mihai-Cojusnean <mihai.cojusnean@gmail.com>
2024-04-13 14:44:36 +0200

    feat: Add sonar cloud.
    This commit adds sonar cloud to  intervene early when coding, allowing to remediate fresh issues rapidly and prevent them from reaching production.

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import {SessionService} from "../../services/session.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-  constructor(private router: Router) {}
+export class HomeComponent implements OnInit {
+  constructor(private router: Router, private session: SessionService) {}
 
   public login() {
     this.router.navigate(['login']).then(() => console.log("Navigated to Login page"));
@@ -15,5 +16,11 @@ export class HomeComponent {
 
   public register() {
     this.router.navigate(['register']).then(() => console.log("Navigated to Register page"));
+  }
+
+  ngOnInit(): void {
+    if (this.session.isLogged) {
+      this.router.navigate(['/posts']).then(() => console.log("Navigated to Posts page"));
+    }
   }
 }
